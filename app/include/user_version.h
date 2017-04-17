0109c5770694788c08d9ef6a90f594a8a91267ed
Tero Marttila <terom@fixme.fi>
2017-04-17 14:14:31 +0300

    build version and modules

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 2.0.0"
+#define NODE_VERSION	"NodeMCU 2.0.0+qmsk-dmx_1"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "unspecified"
+#define BUILD_DATE	  "20170417"
 #endif
 
 extern char SDK_VERSION[];
