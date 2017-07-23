b7a2137b7af56843e6360c27a3c9a1749881948e
Tero Marttila <terom@fixme.fi>
2017-07-23 20:48:03 +0300

    docker-build: make args

--- tools/docker-make.sh
+++ tools/docker-make.sh
@@ -4,4 +4,4 @@ set -uex
 
 docker images -q nodemcu.build || docker build -f Dockerfile.build -t nodemcu.build .
 
-docker run --rm --name nodemcu-build -u $UID -v $PWD:/src -w /src nodemcu.build make
+docker run --rm --name nodemcu-build -u $UID -v $PWD:/src -w /src nodemcu.build make "$@"
