e82b2d41eff288fb5330ae2b9040019c9937ff6d
Tero Marttila <terom@fixme.fi>
2017-07-23 20:08:03 +0300

    tools/docker-make.sh

---
+++ tools/docker-make.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -uex
+
+docker images -q nodemcu.build || docker build -f Dockerfile.build -t nodemcu.build .
+
+docker run --rm --name nodemcu-build -u $UID -v $PWD:/src -w /src nodemcu.build make
