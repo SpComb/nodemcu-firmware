df8292addb7016233c387969158f04973471816c
Tero Marttila <terom@fixme.fi>
2017-07-23 22:15:56 +0300

    docker-build: cat image.bin

--- tools/docker-make.sh
+++ tools/docker-make.sh
@@ -4,4 +4,7 @@ set -uex
 
 docker images -q nodemcu.build || docker build -f Dockerfile.build -t nodemcu.build .
 
-docker run --rm --name nodemcu-build -u $UID -v $PWD:/src -w /src nodemcu.build make "$@"
+docker run --rm --name nodemcu-build -u $UID -v $PWD:/src -w /src nodemcu.build \
+    make "$@"
+docker run --rm --name nodemcu-build -u $UID -v $PWD:/src -w /src nodemcu.build \
+    srec_cat -output bin/image.bin -binary bin/0x00000.bin -binary -fill 0xff 0x00000 0x10000 bin/0x10000.bin -binary -offset 0x10000
